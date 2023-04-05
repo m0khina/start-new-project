3794bfdae56d82aa898c4ffb697df39ed2b1ab2a
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-04-05 16:49:39 -0700

    acc

--- src/pages/Home/Accardeon/openAcc.js
+++ src/pages/Home/Accardeon/openAcc.js
@@ -9,6 +9,7 @@ const OpenAcc = () => {
 
     return (
         <div>
+                <div className="item--div"></div>
             <div onClick={() => {
                 setSelected(!selected)
                 setSelected2(false)
@@ -19,7 +20,6 @@ const OpenAcc = () => {
                     height: selected ? "200px" : "",
                     transition: ".6s"
                 }} className="item">
-                <div className="item--title__div"></div>
                 <div className="item--title">
                     <div>
                         <h4>Сколько всего насчитывается материала всего</h4>
@@ -31,6 +31,7 @@ const OpenAcc = () => {
                     }} className="item--icon" />
                 </div>
             </div>
+            <div className="item--div"></div>
             <div onClick={() => {
                 setSelected2(!selected2)
                 setSelected(false)
@@ -41,7 +42,6 @@ const OpenAcc = () => {
                     height: selected2 ? "200px" : "",
                     transition: ".6s"
                 }} className="item">
-                <div className="item--title__div"></div>
                 <div className="item--title">
                     <div>
                         <h4>Потеряю ли я оставшесея время если перейду на другой до окончания этого?</h4>
@@ -54,6 +54,8 @@ const OpenAcc = () => {
                     }} className="item--icon" />
                 </div>
             </div>
+            <div className="item--div"></div>
+
             <div onClick={() => {
                 setSelected3(!selected3)
                 setSelected(false)
@@ -64,7 +66,6 @@ const OpenAcc = () => {
                     height: selected3 ? "200px" : "",
                     transition: ".6s"
                 }} className="item">
-                <div className="item--title__div"></div>
 
                 <div className="item--title">
 
@@ -78,6 +79,8 @@ const OpenAcc = () => {
                     }} className="item--icon" />
                 </div>
             </div>
+            <div className="item--div"></div>
+
             <div onClick={() => {
                 setSelected4(!selected4)
                 setSelected(false)
@@ -89,17 +92,19 @@ const OpenAcc = () => {
                     transition: ".6s"
                 }} className="item">
                 <div className="item--title">
-                    <div className="item--title__div"></div>
+
+                    <div>
+                        <h4>Откуда мне узнавать про новые статьи и курсы?</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi eius enim
+                            fuga, id laboriosam laudantium magni quidem sed voluptates?</p>
+                    </div>
                     <MdArrowBackIosNew style={{
                         transform: selected4 ? "rotate(90deg)" : ""
                     }} className="item--icon" />
-                    <h4>Откуда мне узнавать про новые статьи и курсы?</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi eius enim
-                        fuga, id laboriosam laudantium magni quidem sed voluptates?</p>
-                    <div style={{ marginTop: "20px" }} className="item--title__div"></div>
                 </div>
 
             </div>
+            <div className="item--div"></div>
         </div>
     );
 };
