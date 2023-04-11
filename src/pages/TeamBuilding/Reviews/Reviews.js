3d2a47a5d1e9ef4b6072ba611a452131aeb672e6
Seitkanov2004 <114854470+Seitkanov2004@users.noreply.github.com>
2023-04-10 23:56:37 -0700

    finished review (#20)
    Co-authored-by: Macbook <macbook@Macbooks-MacBook-Pro.local>

--- src/pages/TeamBuilding/Reviews/Reviews.js
+++ src/pages/TeamBuilding/Reviews/Reviews.js
@@ -1,14 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillStar } from "react-icons/ai";
 
 const Reviews = () => {
+  const [fly, setFly] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
   return (
     <div id="reviewCourse">
       <div className="container">
         <h1 className="reviewCourse--title">Отзывы</h1>
         <div className="reviewCourse">
-            <div className="reviewCourse--block">
-              
+          <div className="reviewCourse--block">
+            <div className="reviewCourse--block__about">
+              <div className="reviewCourse--block__about--user">B</div>
+              <div className="reviewCourse--block__about--name">
+                <h2>Muslim Bolot</h2>
+                <h4>09.09.22</h4>
+                <div className="reviewCourse--block__about--name__fly">
+                  {fly.map((el) => (
+                    <div
+                      className="reviewCourse--block__about--name__fly__item"
+                      key={el}
+                    >
+                      <AiFillStar className="reviewCourse--block__about--name__fly__item--icon" />
+                    </div>
+                  ))}
+                  <span>10/10</span>
+                </div>
+              </div>
             </div>
+            <p>
+              Oрганизованность сайта направление и навигация по материалам. А
+              так же актуальность материалов
+            </p>
+          </div>
+          <div className="reviewCourse--block">
+            <div className="reviewCourse--block__about">
+              <div className="reviewCourse--block__about--user">C</div>
+              <div className="reviewCourse--block__about--name">
+                <h2>Muslim Bolot</h2>
+                <h4>09.09.22</h4>
+                <div className="reviewCourse--block__about--name__fly">
+                  {fly.map((el) => (
+                    <div
+                      className="reviewCourse--block__about--name__fly__item"
+                      key={el}
+                    >
+                      <AiFillStar className="reviewCourse--block__about--name__fly__item--icon" />
+                    </div>
+                  ))}
+                  <span>10/10</span>
+                </div>
+              </div>
+            </div>
+            <p>
+              Oрганизованность сайта направление и навигация по материалам. А
+              так же актуальность материалов
+            </p>
+          </div>
+          <div className="reviewCourse--block">
+            <div className="reviewCourse--block__about">
+              <div className="reviewCourse--block__about--user">M</div>
+              <div className="reviewCourse--block__about--name">
+                <h2>Muslim Bolot</h2>
+                <h4>09.09.22</h4>
+                <div className="reviewCourse--block__about--name__fly">
+                  {fly.map((el) => (
+                    <div
+                      className="reviewCourse--block__about--name__fly__item"
+                      key={el}
+                    >
+                      <AiFillStar className="reviewCourse--block__about--name__fly__item--icon" />
+                    </div>
+                  ))}
+                  <span>10/10</span>
+                </div>
+              </div>
+            </div>
+            <p>
+              Oрганизованность сайта направление и навигация по материалам. А
+              так же актуальность материалов
+            </p>
+          </div>
         </div>
       </div>
     </div>
