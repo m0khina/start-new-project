cae988b0ad5fc941fcf683e650eb4c430d422125
m0khina <artikovamohina.03@gmail.com>
2023-04-11 08:19:56 +0300

    add team-building page

--- src/pages/TeamBuilding/TeamBuilding.js
+++ src/pages/TeamBuilding/TeamBuilding.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import AboutCourse from "./AboutCourse/AboutCourse";
 import HeroTeam from "./HeroTeam/HeroTeam";
+import CourseInfo from "./CourseInfo/CourseInfo";
+import CourseProgramme from "./CourseProgramme/CourseProgramme";
+import FollowCourse from "./FollowCourse/FollowCourse";
 
 const TeamBuilding = () => {
     return (
         <div>
             <HeroTeam/>
             <AboutCourse/>
+            <CourseInfo/>
+            <CourseProgramme/>
+            <FollowCourse/>
         </div>
     );
 };
