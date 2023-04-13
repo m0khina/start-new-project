4ed9a2fd37822bf6fa87b6888adfb86b1a16d710
ramazan784dev <rsaparbekov784@gmail.com>
2023-04-13 07:27:08 +0600

    add master-class pages

--- src/pages/TeamBuilding/HeroTeam/HeroTeam.js
+++ src/pages/TeamBuilding/HeroTeam/HeroTeam.js
@@ -13,15 +13,16 @@ const HeroTeam = () => {
       <div className="container">
         <div className="hero">
           <img src={Book} alt="" className="hero--bgBook" />
-          <p className="hero--pTitle">Наши курсы / Курсы по созданию команды</p>
           <div className="hero--titles">
             <img src={Book} alt="" className="hero--titles__book1" />
             <div className="hero--titles__greatTeam">
+              <p className="hero--titles__greatTeam--pTitle">Наши курсы / Курсы по созданию команды</p>
               <h1>Создание команды</h1>
               <p>
                 Как правильно строить команду и эффективно ею управлять, чтобы
                 достигать все поставленные цели.
               </p>
+              <button className="hero--btn">Получить доступ за 500,00 $</button>
             </div>
             <div className="hero--titles__insayt">
               <h3>В курс входит</h3>
@@ -29,7 +30,6 @@ const HeroTeam = () => {
               <p>100% эксклюзив</p>
             </div>
           </div>
-          <button className="hero--btn">Получить доступ за 500,00 $</button>
         </div>
         {openVidoe ? (
           <iframe
