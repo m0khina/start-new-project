60e913121998797dde51cb1499a8e002c3bd0340
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-04-14 16:53:09 -0700

    ask

---
+++ src/fakeBackEnd/FakeHomeBook/Index.js
@@ -0,0 +1,39 @@
+// export const HeroBook = {
+//   tabs: [
+//     {
+//       id: 1,
+//       title: "Все разделы",
+//       isDone: false,
+//     }, 
+//     {
+//       id: 2,
+//       title: "Новое",
+//       isDone: false,
+//     }, 
+//     {
+//       id: 3,
+//       title: "Софт-скилс",
+//       isDone: false,
+//     }, 
+//     {
+//       id: 4,
+//       title: "Книги",
+//       isDone: false,
+//     }, 
+//     {
+//       id: 5,
+//       title: "Окружение",
+//       isDone: false,
+//     },
+//   ],
+
+//   books: [
+//     [
+//         id: 1,
+//         isDone,
+//         {
+
+//         },
+//     ],
+//   ]
+// };
