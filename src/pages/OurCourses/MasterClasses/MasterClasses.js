9b0914129cbf80db754f8c3ea9314dcc5c00fb65
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-04-15 11:52:49 -0700

    fake back

--- src/pages/OurCourses/MasterClasses/MasterClasses.js
+++ src/pages/OurCourses/MasterClasses/MasterClasses.js
@@ -1,115 +1,45 @@
-import React from 'react';
-import "./MasterClasses.scss"
-import MK from "./../../../assets/img/master-klass-img.png"
-import Book from "./../../../assets/img/logo.svg"
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import "./MasterClasses.scss";
+import MK from "./../../../assets/img/master-klass-img.png";
+import Book from "./../../../assets/img/logo.svg";
+import { NavLink } from "react-router-dom";
+import { OurApi } from "../../../fakeBackEnd/OurBack";
 
 const MasterClasses = () => {
-    return (
-        <div id="mk">
-            <div className="container">
-                <div className="mk">
-                    <img src={Book} alt="" className="mk--book"/>
-                    <h2 className="mk--title">Мастер классы</h2>
-                    <div className="mk--cards">
-                        <NavLink to={"/team-building"} >
-                        <div className="mk--card">
-                            <h3>Создание команды</h3>
-                            <div className="mk--card__info">
-                                <h4>Обучение</h4>
-                                <div className="mk--card__info--btn">
-                                    <button>Все</button>
-                                    <button>Новое</button>
-                                    <button>Книги</button>
-                                </div>
-                            </div>
-                            <img src={MK} alt="" className="mk--card__imgMk"/>
-                            <a href="">Подробнее...</a>
-                        </div>
-                        </NavLink>
-                        <NavLink to={"/team-building"}>
-                        <div className="mk--card">
-                            <h3>Создание команды</h3>
-                            <div className="mk--card__info">
-                                <h4>Обучение</h4>
-                                <div className="mk--card__info--btn">
-                                    <button>Все</button>
-                                    <button>Новое</button>
-                                    <button>Книги</button>
-                                </div>
-                            </div>
-                            <img src={MK} alt="" className="mk--card__imgMk"/>
-                            <a href="">Подробнее...</a>
-                        </div>
-                        </NavLink>
-                        <NavLink to={"/team-building"}>
-                        <div className="mk--card">
-                            <h3>Создание команды</h3>
-                            <div className="mk--card__info">
-                                <h4>Обучение</h4>
-                                <div className="mk--card__info--btn">
-                                    <button>Все</button>
-                                    <button>Новое</button>
-                                    <button>Книги</button>
-                                </div>
-                            </div>
-                            <img src={MK} alt="" className="mk--card__imgMk"/>
-                            <a href="">Подробнее...</a>
-                        </div>
-                        </NavLink>
-                        <NavLink to={"/team-building"}>
-                        <div className="mk--card">
-                            <h3>Создание команды</h3>
-                            <div className="mk--card__info">
-                                <h4>Обучение</h4>
-                                <div className="mk--card__info--btn">
-                                    <button>Все</button>
-                                    <button>Новое</button>
-                                    <button>Книги</button>
-                                </div>
-                            </div>
-                            <img src={MK} alt="" className="mk--card__imgMk"/>
-                            <a href="">Подробнее...</a>
-                        </div>
-                        </NavLink>
-                        <NavLink to={"/team-building"}>
-                        <div className="mk--card">
-                            <h3>Создание команды</h3>
-                            <div className="mk--card__info">
-                                <h4>Обучение</h4>
-                                <div className="mk--card__info--btn">
-                                    <button>Все</button>
-                                    <button>Новое</button>
-                                    <button>Книги</button>
-                                </div>
-                            </div>
-                            <img src={MK} alt="" className="mk--card__imgMk"/>
-                            <a href="">Подробнее...</a>
-                        </div>
-                        </NavLink>
-                        <NavLink to={"team-building"}>
-                        <div className="mk--card">
-                            <h3>Создание команды</h3>
-                            <div className="mk--card__info">
-                                <h4>Обучение</h4>
-                                <div className="mk--card__info--btn">
-                                    <button>Все</button>
-                                    <button>Новое</button>
-                                    <button>Книги</button>
-                                </div>
-                            </div>
-                            <img src={MK} alt="" className="mk--card__imgMk"/>
-                            <a href="">Подробнее...</a>
-                        </div>
-                        </NavLink>
+  return (
+    <div id="mk">
+      <div className="container">
+        <div className="mk">
+          <img src={Book} alt="" className="mk--book" />
+          <h2 className="mk--title">Мастер классы</h2>
+          <div className="mk--cards">
+            {OurApi.map((el) => (
+              <NavLink to={"/team-building"}>
+                <div className="mk--card">
+                  <h3>{el.title}</h3>
+                  <div className="mk--card__info">
+                    <h4>{el.learn}</h4>
+                    <div className="mk--card__info--btn">
+                      {
+                        el.buttons.map(btn => (
+                            <button>{btn.title}</button>
+                        ))
+                      }
                     </div>
-                    <center>
-                        <button className="mk--btnMain">Смотреть все мастер классы</button>
-                    </center>
+                  </div>
+                  <img src={el.image} alt="" className="mk--card__imgMk" />
+                  <a href="">Подробнее...</a>
                 </div>
-            </div>
+              </NavLink>
+            ))}
+          </div>
+          <center>
+            <button className="mk--btnMain">Смотреть все мастер классы</button>
+          </center>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MasterClasses;
